c35177e3275ad5b72fc2ebff5eb389cc454d4315
cnuzillat <cnuzillat@gmail.com>
2026-02-17 17:50:23 -0500

    Implement offset recovery on startup to preserve log continuity

--- src/main/java/com/cnuzillat/logstream/storage/LogSegment.java
+++ src/main/java/com/cnuzillat/logstream/storage/LogSegment.java
@@ -16,6 +16,33 @@ public class LogSegment {
     public LogSegment(Path path) throws IOException {
         this.channel = FileChannel.open(path, StandardOpenOption.READ, StandardOpenOption.WRITE,
                 StandardOpenOption.CREATE);
+
+        recoverOffset();
+    }
+
+    private void recoverOffset() throws IOException {
+        channel.position(0);
+
+        ByteBuffer header = ByteBuffer.allocate(12);
+
+        long lastOffset = -1;
+
+        while (true) {
+            header.clear();
+            int read = channel.read(header);
+            if (read < 12) {
+                break;
+            }
+            header.flip();
+            int length = header.getInt();
+            long offset = header.getLong();
+
+            channel.position(channel.position() + length);
+
+            lastOffset = offset;
+        }
+
+        nextOffset = lastOffset + 1;
     }
 
     public synchronized long append(byte[] payload) throws IOException {
@@ -31,6 +58,7 @@ public class LogSegment {
         while (buffer.hasRemaining()) {
             channel.write(buffer);
         }
+
         return currentOffset;
     }
 
@@ -59,7 +87,7 @@ public class LogSegment {
 
             byte[] payload = new byte[length];
             payloadBuffer.get(payload);
-            records.add(Arrays.toString(payload) + ": " + new String(payload));
+            records.add(offset + ": " + new String(payload));
         }
         return records;
     }
